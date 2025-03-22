00c30a4528c1f0189550b35ba41f109eb3338fd0
Yaswanth Narayana Pilla <nagayaswanth.p@inncircles.com>
2025-03-22 08:20:01 +0530

    contact form integrated

--- src/app/routes/contact-me/contact-me.component.ts
+++ src/app/routes/contact-me/contact-me.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-contact-me',
-  standalone: true,
-  imports: [],
-  templateUrl: './contact-me.component.html',
-  styleUrl: './contact-me.component.scss'
+	selector: 'app-contact-me',
+	standalone: true,
+	imports: [
+		ReactiveFormsModule
+	],
+	templateUrl: './contact-me.component.html',
+	styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
+	contactForm !: FormGroup
 
+	constructor(private fb : FormBuilder) {
+		this.contactForm = fb.group({
+			name : new FormControl<string>('',[Validators.required]),
+			email : new FormControl<string>('',[Validators.required, Validators.email]),
+			message : new FormControl<string>('',[Validators.required])
+		})
+	}
+
+
+	sendMessage() {
+		if(this.contactForm.valid) {
+
+		} else {
+
+		}
+	}
 }
