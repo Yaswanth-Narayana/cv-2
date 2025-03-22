5af66bad9c93b91c1af1579476911e1dca1a5c8f
Yaswanth Narayana Pilla <nagayaswanth.p@inncircles.com>
2025-03-22 12:03:01 +0530

    update  build

--- chunk-3R4HYHXX.js
+++
@@ -1,1 +0,0 @@
-import{Ra as n,Sa as s,W as o,cb as a,ib as r}from"./chunk-LMRLHKNS.js";var p=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-projects"]],standalone:!0,features:[r],decls:2,vars:0,template:function(e,i){e&1&&(n(0,"p"),a(1,"projects works!"),s())}})};export{p as ProjectsComponent};
