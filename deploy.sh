4c8aa5109067980a430b2bbe9ff3338f32b39283
Yaswanth Narayana Pilla <nagayaswanth.p@inncircles.com>
2025-03-22 15:04:36 +0530

    should work

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 set -e 
 
 # Define variables
-DIST_DIR="dist"  # Update with your Angular project name inside dist
-TEMP_DIR="../temp-deploy"           # Temporary storage for dist files
+DIST_DIR="dist/cv-2/browser/"  
+TEMP_DIR="../temp-deploy"
 BRANCH_TO_DEPLOY="master"
 
 echo "installing node modules..."
@@ -14,31 +14,33 @@ echo "Building Angular project..."
 ng build
 
 echo "Creating temporary folder..."
-rm -rf $TEMP_DIR && mkdir $TEMP_DIR && cp -r $DIST_DIR/cv-2/browser* $TEMP_DIR
+rm -rf $TEMP_DIR && mkdir $TEMP_DIR && cp -r $DIST_DIR* $TEMP_DIR
 
-echo "Switching to $BRANCH_TO_DEPLOY branch..."
-git stash --include-untracked
-git checkout $BRANCH_TO_DEPLOY
+echo ""
 
-echo "Deleting all files except .gitignore..."
-git ls-files | grep -v '.gitignore' | xargs git rm -rf
+# echo "Switching to $BRANCH_TO_DEPLOY branch..."
+# git stash --include-untracked
+# git checkout $BRANCH_TO_DEPLOY
 
-echo "Moving new dist files from temporary folder..."
-cp -r $TEMP_DIR/* .
+# echo "Deleting all files except .gitignore..."
+# git ls-files | grep -v '.gitignore' | xargs git rm -rf
+
+# echo "Moving new dist files from temporary folder..."
+# cp -r $TEMP_DIR/* .
 
 # echo "Cleaning up temporary folder..."
 # rm -rf $TEMP_DIR
 
-echo "Adding changes to $BRANCH_TO_DEPLOY branch..."
-git add .
-git commit -m "Deploy: $(date)"
+# echo "Adding changes to $BRANCH_TO_DEPLOY branch..."
+# git add .
+# git commit -m "Deploy: $(date)"
 
-echo "Pushing to $BRANCH_TO_DEPLOY branch..."
-git push origin $BRANCH_TO_DEPLOY
+# echo "Pushing to $BRANCH_TO_DEPLOY branch..."
+# git push origin $BRANCH_TO_DEPLOY
 
-echo "Switching back to main branch..."
-git checkout main
-git stash pop
+# echo "Switching back to main branch..."
+# git checkout main
+# git stash pop
 
 echo "Deployment successful! 🚀"
 
